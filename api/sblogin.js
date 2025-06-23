39afe037216a26298a4bacdd670869af6f1ca37b
bakhtiar9011 <37062612+bakhtiar9011@users.noreply.github.com>
2025-06-23 17:04:42 +0500

    changes

--- api/sblogin.js
+++ api/sblogin.js
@@ -5,19 +5,19 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    if (req.method === 'OPTIONS') {
-        res.status(200).end(); // CORS preflight
-        return;
-    }
-    
-    const { email, password } = req.body;
+    // if (req.method === 'OPTIONS') {
+    //     res.status(200).end(); // CORS preflight
+    //     return;
+    // }
 
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password
-    });
+    // const { email, password } = req.body;
 
-    if (error) return res.status(401).json({ error: error.message });
+    // const { data, error } = await supabase.auth.signInWithPassword({
+    //     email,
+    //     password
+    // });
 
-    return res.status(200).json({ session: data.session });
+    // if (error) return res.status(401).json({ error: error.message });
+
+    return res.status(200).json({ status: "success" });
 }
