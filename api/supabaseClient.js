cfe0f5ad9f858dd73c855c3c85d66673627b166e
bakhtiar9011 <37062612+bakhtiar9011@users.noreply.github.com>
2025-06-24 16:36:42 +0500

    change

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qgzytdvnjnuwbhpvndyn.supabase.co';
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // Use secure key from Vercel env vars
+const supabaseKey = process.env.SUPABASE_ANON_KEY; // Use secure key from Vercel env vars
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
